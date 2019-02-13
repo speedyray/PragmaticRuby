3d6f75b88d2949be5f9e3a693577cad777aa03e3
Raynold Gyasi <speedyray2ray@gmail.com>
2019-02-13 12:42:16 -0500

    Add numbers ans strings

--- studio_game.rb
+++ studio_game.rb
@@ -1,16 +1,33 @@
 
-puts "Welcome"
+# puts "Welcome"
+
+# greeting = "Welcome"
+
+# 3.times do
+#  puts greeting.upcase	
+# end
+
+# puts Time.new
+
+# puts Time.now
 
 
-greeting = "Welcome"
+first_player = "larry"
+larry_health   = 60
 
-3.times do
- puts greeting.upcase	
-end
+second_player ="curly"
+
+third_player ="moe"
+
+puts "#{first_player}'s health is #{larry_health * 3}"
+
+puts "#{first_player}'s health is #{larry_health / 9.0}"
+
+puts "#{first_player}'s health is #{larry_health /  9 }"
+
+puts "Players: \n\tlarry \n\tcurly \n\tmoe"
+
+puts "Players: \n\t#{first_player}\n\t#{second_player}\n\t#{third_player}"
 
 
 
-puts Time.new
-
-
-puts Time.now
