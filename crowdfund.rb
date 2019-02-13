c798e0f052db6d38a93874d9aed5a1fc09f638a6
Raynold Gyasi <speedyray2ray@gmail.com>
2019-02-13 12:53:26 -0500

    Add crowdfund

---
+++ crowdfund.rb
@@ -0,0 +1,12 @@
+project1 = "ABC"
+project2 = "LMN"
+project3 = "XYZ"
+
+
+puts "Project #{project1} has $1000 in funding"
+puts "Project #{project2} has $5000 in funding"
+puts "Project #{project3} has $10000 in funding"
+
+
+
+puts "Projects: \n\tProject ABC\n\tProject LMN\n\tProject XYZ"
